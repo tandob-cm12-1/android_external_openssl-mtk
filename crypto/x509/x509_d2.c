1fd4874c83f7d6b4006d90d541c67a6245d29fe8
fire855 <thefire855@gmail.com>
2015-05-26 22:58:16 +0200

    Add AGPS Mediatek changes
    Change-Id: Ie7dd90ad82ad553eea2406a5878c2af0515c9a5d

--- crypto/x509/x509_d2.c
+++ crypto/x509/x509_d2.c
@@ -104,4 +104,53 @@ int X509_STORE_load_locations(X509_STORE *ctx, const char *file,
 	return(1);
 	}
 
+#ifdef MTK_HARDWARE
+// MTK AGPS Support
+int X509_STORE_set_default_paths_ext(X509_STORE *ctx, int filetype)
+	{
+	X509_LOOKUP *lookup;
+
+    if (filetype != X509_FILETYPE_ASN1)
+        return X509_STORE_set_default_paths(ctx);
+       
+	lookup=X509_STORE_add_lookup(ctx,X509_LOOKUP_file());
+	if (lookup == NULL) return(0);
+	X509_LOOKUP_load_file(lookup,NULL,X509_FILETYPE_ASN1);
+
+	lookup=X509_STORE_add_lookup(ctx,X509_LOOKUP_hash_dir());
+	if (lookup == NULL) return(0);
+	X509_LOOKUP_add_dir(lookup,NULL,X509_FILETYPE_ASN1);
+	
+	/* clear any errors */
+	ERR_clear_error();
+	return(1);
+	}
+
+int X509_STORE_load_locations_ext(X509_STORE *ctx, const char *file,
+		const char *path, int filetype)
+	{
+	X509_LOOKUP *lookup;
+
+    if (filetype != X509_FILETYPE_ASN1)
+        return X509_STORE_load_locations(ctx, file, path);
+
+	if (file != NULL)
+		{
+		lookup=X509_STORE_add_lookup(ctx,X509_LOOKUP_file());
+		if (lookup == NULL) return(0);
+		if (X509_LOOKUP_load_file(lookup,file,X509_FILETYPE_ASN1) != 1)
+		    return(0);
+		}
+	if (path != NULL)
+		{
+		lookup=X509_STORE_add_lookup(ctx,X509_LOOKUP_hash_dir());
+		if (lookup == NULL) return(0);
+		if (X509_LOOKUP_add_dir(lookup,path,X509_FILETYPE_ASN1) != 1)
+		    return(0);
+		}
+	if ((path == NULL) && (file == NULL))
+		return(0);
+	return(1);
+	}
+#endif
 #endif
